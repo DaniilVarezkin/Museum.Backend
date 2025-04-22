8124ab67e2261a669467a5b90277e9a53c7cf0b0
Daniil Varezkin <daniil.varezkin@gmail.com>
2025-04-22 16:33:13 +0300

    Add project files.

---
+++ Domain/EventPhoto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Domain
+{
+    public class EventPhoto
+    {
+        public Guid Id { get; set; }
+        public string FilePath { get; set; } = string.Empty;
+        public MuseumEvent? MuseumEvent { get; set; } = null;
+
+        [ForeignKey(nameof(MuseumEvent))]
+        public Guid MuseumEventId { get; set; }
+    }
+}
---
+++ Domain/MuseumEvent.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class MuseumEvent
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Annotation { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public required AudienceType AudienceType {  get; set; }
+        public required MuseumEventType EventType {  get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
+        public string TicketLink { get; set; } = string.Empty;
+        public ICollection<EventPhoto> Photos { get; set; } = new List<EventPhoto>();
+    }
+}
