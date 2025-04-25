171d079c8f5f56090b0e75bea9de604c43a07910
Daniil Varezkin <daniil.varezkin@gmail.com>
2025-04-25 21:43:13 +0300

    - Добавлен интерфейс IMapWith<T> с методом ConfigureMapping(Profile profile) для настройки сопостовления типа T с классом, реализующим интерфейс. - Добавлен класс AssemlblyMappingProfile, применяющий настройки сопоставления для всех классов реализующих интерфейс IMapWith<T>.

---
+++ Application/Common/Mapping/AssemblyMappingProfile.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using System.Reflection;
+
+namespace Museum.Application.Common.Mapping
+{
+    public class AssemblyMappingProfile : Profile
+    {
+        public AssemblyMappingProfile(Assembly assembly) =>
+            ApplyMappingConfigurationFromAssembly(assembly);
+
+        private void ApplyMappingConfigurationFromAssembly(Assembly assembly)
+        {
+            var types = assembly.GetExportedTypes()
+                .Where(t => t.GetInterfaces()
+                    .Any(i => i.IsGenericType && 
+                         i.GetGenericTypeDefinition() == typeof(IMapWith<>))
+                    ).ToList();
+
+            foreach ( var type in types )
+            {
+                var instance = Activator.CreateInstance(type);
+                var methodInfo = type.GetMethod(nameof(IMapWith<object>.ConfigureMapping));
+
+                methodInfo?.Invoke(instance, [this]);
+            }
+        }
+    }
+}
---
+++ Application/Common/Mapping/IMapWith.cs
@@ -0,0 +1,13 @@
+﻿
+using AutoMapper;
+
+namespace Museum.Application.Common.Mapping
+{
+    public interface IMapWith<T>
+    {
+        void ConfigureMapping(Profile profile)
+        {
+            profile.CreateMap(typeof(T), GetType());
+        }
+    }
+}
